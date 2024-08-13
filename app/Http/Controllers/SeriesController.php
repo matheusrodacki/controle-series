88a641b288b76002528dba0ccc7d786355e18f6a
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-08-13 05:59:30 +0000

    adiciona rota para series

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = [
+            'The Walking Dead',
+            'Game of Thrones',
+            'Breaking Bad'
+        ];
+
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+
+        return $html;
+    }
+}
