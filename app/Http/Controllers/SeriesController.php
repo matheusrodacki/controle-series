e02aa92dc2851e5a154eb447237cf41407f2e83c
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-08-13 06:29:44 +0000

    implementa layout e compoents do blade

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -14,6 +14,6 @@ class SeriesController extends Controller
             'Breaking Bad'
         ];
 
-        return view('listar-series', compact('series'));
+        return view('series.index', compact('series'));
     }
 }
