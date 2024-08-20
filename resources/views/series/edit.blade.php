37e05cb09b9d5a4b8ef97716a582d833d60eef69
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-08-20 17:45:03 -0300

    feat: :sparkles: add seasons view

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,20 +1,22 @@
 <x-layout title="Editar Série  '{!! $series->name !!}'">
-    <form action="{{ route('series.update') }}" method="post">
+    <form action="{{ route('series.update', $series->id) }}" method="post">
         @csrf
         @method('PUT')
         <div class="row mb-3">
             <div class="col-8">
                 <label for="name" class="form-label">Nome da Série:</label>
-                <input type="text" name="name" id="name" class="form-control" value="{{ $serie->name }}"
+                <input type="text" name="name" id="name" class="form-control" value="{{ $series->name }}"
                     autofocus>
             </div>
             <div class="col-2">
                 <label for="seasonsQty" class="form-label">Número de Temporadas:</label>
-                <input type="text" name="seasonsQty" id="seasonsQty" class="form-control">
+                <input type="text" name="seasonsQty" id="seasonsQty" class="form-control"
+                    value="{{ $series->seasons->count() }}">
             </div>
             <div class="col-2">
                 <label for="episodesPerSeason" class="form-label">Episodios por Temporada:</label>
-                <input type="text" name="episodesPerSeason" id="episodesPerSeason" class="form-control">
+                <input type="text" name="episodesPerSeason" id="episodesPerSeason" class="form-control"
+                    value="{{}}">
             </div>
         </div>
         <button type="submit" class="btn btn-dark mt-2">
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,11 +7,13 @@
         </div>
     @endisset
 
-
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-itens-center">
-                {{ $serie->name }}
+                <a class="link-dark link-underline link-underline-opacity-0"
+                    href="{{ route('seasons.index', $serie->id) }}">
+                    {{ $serie->name }}
+                </a>
                 <span class="d-flex">
                     <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">E</a>
                     <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
